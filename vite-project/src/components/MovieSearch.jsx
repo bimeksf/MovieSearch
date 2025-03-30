efc83559d761430eadb0bfcfb3f5b90cba91f341
bimeksf <d3disek@gmail.com>
2025-03-30 23:00:42 +0200

    final

--- vite-project/src/components/MovieSearch.jsx
+++ vite-project/src/components/MovieSearch.jsx
@@ -1,69 +1,66 @@
-import { useState , useEffect,useCallback } from "react";
+import { useState, useEffect, useCallback } from "react";
 
 import Movie from "./Movie";
 export default function MovieSearch() {
-
-    const [movie , setMovie] = useState([])
-    const [query , setQuery] = useState("")
+  const [movie, setMovie] = useState([]);
+  const [query, setQuery] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const apikey = import.meta.env.VITE_API_KEY;
 
-
-
-
-
   const searchMovie = useCallback(async () => {
-    if(query.length <3)
-      
-  {    setMovie([])
-      
-      return}
-  
+    if (query.length < 3) {
+      setMovie([]);
+
+      return;
+    }
+    setLoading(true);
     const url = `https://api.themoviedb.org/3/search/movie?api_key=${apikey}&language=en-US&query=${query}&page=1&include_adult=false`;
-  
+
     try {
       const response = await fetch(url);
-  
+
       const data = await response.json();
-  
-      setMovie(data.results || [])
-  
+
+      setMovie(data.results || []);
+
       console.log(data);
     } catch (err) {
       console.error(err);
+    } finally {
+      setLoading(false);
     }
   }, [query, apikey]);
   useEffect(() => {
     const debounce = setTimeout(() => {
       searchMovie();
-    }, 400);
+    }, 500);
 
     return () => clearTimeout(debounce);
-  }, [query,searchMovie ]);
+  }, [query, searchMovie]);
 
+  const movies = movie
+    .filter((movie) => movie.poster_path)
+    .map((movie) => <Movie key={movie.id} movie={movie} />);
 
-
-
-  const movies = movie.filter( movie => movie.poster_path).map((movie)=>< Movie key={movie.id}  movie={movie} />)
-
-function handleSubmit(e){
-  e.preventDefault()
-  searchMovie()
-
-}
-
+  function handleSubmit(e) {
+    e.preventDefault();
+    searchMovie();
+  }
 
   return (
     <>
-
-      <form onSubmit={handleSubmit} className="flex justify-center items-center p-4 gap-1">
-       
-          <input
-            type="text"
-            placeholder="Search for a Movie.."
-            className="p-3 border-1 border-stone-500 bg-stone-800   outline-none rounded-md focus:outline-4 outline-white  text-white "
-            value={query} onChange={(e)=>setQuery(e.target.value)}
-          />
+      <form
+        onSubmit={handleSubmit}
+        className="flex justify-center items-center p-4 gap-1"
+      >
+        <input
+          type="text"
+          placeholder="Search for a Movie.."
+          className="p-3 border-1 border-stone-500 bg-stone-800   outline-none rounded-md focus:outline-4 outline-white  text-white "
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
 
         <button
           type="submit"
@@ -72,13 +69,15 @@ function handleSubmit(e){
           search
         </button>
       </form>
-        <div className="flex  justify-center items-center gap-4 flex-wrap">
-        {movie.length  > 0 ?   movies : <p className="text-lg text-white mt-7">No Movie Found </p>  }
-
-
-        </div>
-
-
+      <div className="flex  justify-center items-center gap-4 flex-wrap">
+        {loading ? (
+          <p className="text-lg text-white mt-7">Loading...</p> // Pokud se filmy načítají
+        ) : movies.length > 0 ? (
+          movies
+        ) : (
+          <p className="text-lg text-white mt-7">No Movie Found</p> // Pokud nejsou žádné filmy
+        )}
+      </div>
     </>
   );
 }
